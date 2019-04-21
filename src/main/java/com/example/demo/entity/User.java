307870f3c3f384d9a39348bdfec2455131c2a632
zhouxu <364427701@qq.com>
2019-04-21 11:53:53 +0800

    first commit

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.demo.entity;
+
+import java.util.UUID;
+import lombok.Data;
+
+@Data
+class User {
+    private UUID id;
+
+    private String userName;
+
+    private String authority;
+
+    private String password;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                '}';
+    }
+}
