d6c04e1d36852f63f875ee3b9a09b05010028d2e
zhouxu <zhouxu@huas.space>
2019-04-23 18:19:55 +0800

    尝试写了下登录

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    @ResponseBody
+    String index() {
+        return "Hello World!";
+    }
+
+    @RequestMapping("/welcome")
+    ModelAndView welcome(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("welcome");
+        modelAndView.addObject("message","Welcome to Spring Boot & Freemarker");
+        return modelAndView;
+    }
+}
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,14 +3,14 @@ package com.example.demo.controller;
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping(value = "/user", method = { RequestMethod.GET, RequestMethod.POST })
 public class UserController {
     @Autowired
