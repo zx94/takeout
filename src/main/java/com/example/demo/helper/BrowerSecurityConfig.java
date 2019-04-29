49967466e2c757a238b4625343992e7c51822bc3
zhouxu <364427701@qq.com>
2019-04-29 22:50:11 +0800

    转换成thymeleaf引擎

--- src/main/java/com/example/demo/helper/BrowerSecurityConfig.java
+++ src/main/java/com/example/demo/helper/BrowerSecurityConfig.java
@@ -12,11 +12,11 @@
 //    @Override
 //    protected void configure(HttpSecurity http) throws Exception {
 //        http.formLogin()                    //  定义当需要用户登录时候，转到的登录页面。
-//                .loginPage("/login.ftl")           // 设置登录页面
+//                .loginPage("/login.html")           // 设置登录页面
 //                .loginProcessingUrl("/user/login")  // 自定义的登录接口
 //                .and()
 //                .authorizeRequests()        // 定义哪些URL需要被保护、哪些不需要被保护
-//                .antMatchers("/login.ftl").permitAll()     // 设置所有人都可以访问登录页面
+//                .antMatchers("/login.html").permitAll()     // 设置所有人都可以访问登录页面
 //                .anyRequest()               // 任何请求,登录后可以访问
 //                .authenticated()
 //                .and()
